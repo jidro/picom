daa9016b54392b0a8410b4462c76719cf48b932d
Jidro.Z <zoujizhuo524@163.com>
2023-03-27 16:41:12 +0800

    Consolidate and streamline my own configuration.

--- my-fast/reduction.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-# Rename the existing picom.conf file to picom.myfast.conf.
-mv ~/.config/picom/picom.conf ~/.config/picom/picom.myfast.conf
-
-# Restore the previously backed up files.
-mv ~/.config/picom/picom.conf.bak ~/.config/picom/picom.conf
-
-# Restart the picom.
-picom -CGb &
